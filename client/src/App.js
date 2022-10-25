e89cc8dd38cb55db4c1dbad25c6eba1c549f3064
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-10-25 16:00:51 -0400

    Added test routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect} from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,21 +14,22 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+          
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path="/testing">
+              <h1>Test Route</h1>
+            </Route>
+            <Route path="/">
+              <h1>Page Count: {count}</h1>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
